5860d209b806245e98096b99764144d5cb27f817
tirthachetry-byte <tirtha.chetry@thoughtworks.com>
2026-06-10 17:41:25 +0530

    Update ProductService.java

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -9,6 +9,7 @@ public class ProductService {
         return "Product details for ID: " + id;
     }
 
+    
     public String getProductDetailsByName(String name) {
         return "Product details for Name: " + name;
     }
